134c7d1c53dbe910746f62d549884b7e6ee90a51
Tyler Long <tyler4long@gmail.com>
2018-06-26 16:37:50 +0800

    Fix js syntax error

--- lightning-component/MyWebAppComponentController.js
+++ lightning-component/MyWebAppComponentController.js
@@ -6,25 +6,25 @@
     window.receiveMessage = function(event) {
       switch(event.data.type) {
         case 'WRITE_CUSTOM_STRING':
-          const action = component.get("c.setOpportunityCustomString");
-          action.setParams({
+          const setAction = component.get("c.setOpportunityCustomString");
+          setAction.setParams({
             opportunityId: component.get("v.recordId"),
             customString: event.data.customString
           });
-          $A.enqueueAction(action);
+          $A.enqueueAction(setAction);
           console.log('Writing custom string', event.data.customString)
           break
         case 'READ_CUSTOM_STRING':
-          const action = component.get("c.getOpportunityCustomString");
-          action.setParams({
+          const getAction = component.get("c.getOpportunityCustomString");
+          getAction.setParams({
             opportunityId: component.get("v.recordId")
           });
-          action.setCallback(this, function(response) {
+          getAction.setCallback(this, function(response) {
             const customString = response.getReturnValue();
             document.getElementById('my-web-app-iframe').contentWindow
               .postMessage({ type: 'CUSTOM_STRING_FROM_SALESFORCE', customString }, '*')
           });
-          $A.enqueueAction(action);
+          $A.enqueueAction(getAction);
           console.log('Reading custom string', event.data.customString)
           break
         default:
