13cbd20f18f835a7c7398c91bc3b7a575f5f3e8f
Tyler Long <tyler4long@gmail.com>
2018-06-26 16:07:04 +0800

    Ready for testing version

--- lightning-component/MyWebAppComponentController.js
+++ lightning-component/MyWebAppComponentController.js
@@ -1,29 +1,36 @@
 ({
   doInit : function(component, event, helper) {
-      let action = component.get("c.getOpportunityGlipGroupId");
-      action.setParams({
-          opportunityId: component.get("v.recordId")
-      });
-      action.setCallback(this, function(response) {
-          let glipGroupId = response.getReturnValue();
-          component.set("v.glipGroupId", glipGroupId);
-      });
-      $A.enqueueAction(action);
-      
-      if(window.receiveMessage !== undefined){
-          window.removeEventListener("message", window.receiveMessage);
+    if(window.receiveMessage !== undefined){
+      window.removeEventListener("message", window.receiveMessage);
+    }
+    window.receiveMessage = function(event) {
+      switch(event.data.type) {
+        case 'WRITE_CUSTOM_STRING':
+          const action = component.get("c.setOpportunityCustomString");
+          action.setParams({
+            opportunityId: component.get("v.recordId"),
+            customString: event.data.customString
+          });
+          $A.enqueueAction(action);
+          console.log('Writing custom string', event.data.customString)
+          break
+        case 'READ_CUSTOM_STRING':
+          const action = component.get("c.getOpportunityCustomString");
+          action.setParams({
+            opportunityId: component.get("v.recordId")
+          });
+          action.setCallback(this, function(response) {
+            const customString = response.getReturnValue();
+            document.getElementById('my-web-app-iframe').contentWindow
+              .postMessage({ type: 'CUSTOM_STRING_FROM_SALESFORCE', customString }, '*')
+          });
+          $A.enqueueAction(action);
+          console.log('Reading custom string', event.data.customString)
+          break
+        default:
+          break
       }
-      window.receiveMessage = function(event) {
-          if(event.data.type === 'WRITE_CUSTOM_STRING') {
-              let action = component.get("c.setOpportunityCustomString");
-              action.setParams({
-                  opportunityId: component.get("v.recordId"),
-                  customString: event.data.customString
-              });
-              $A.enqueueAction(action);
-              console.log('Saving custom string', event.data.customString)
-          }
-      }
-      window.addEventListener("message", receiveMessage);
+    }
+    window.addEventListener("message", receiveMessage);
   }
 })
